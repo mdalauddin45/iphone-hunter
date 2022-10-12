58173299ef272ea0b46fd193319d1aba644e24bb
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-12 21:48:37 +0600

    d

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
+const phones = require("./phone.json");
 const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("look hlw Iphone server runing!");
 });
+
+app.get("/phones", (req, res) => {
+  res.send(phones);
+});
+
+app.get("/phones/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log("looking for data for id:", id);
+
+  const phone = phones.find((ph) => ph.id === id) || {};
+  res.send(phone);
+});
+
 app.listen(port, () => {
   console.log(`iphone server on runing on port ${port}!`);
 });
